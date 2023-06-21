20365fe5a57509e6f9bb1539a12570fd70af406d
Chris Martin <ch.martin@gmail.com>
2023-06-21 13:08:17 -0600

    raise versions, add flake

---
+++ nix/default.nix
@@ -0,0 +1,59 @@
+{ pkgs }:
+
+let
+  inherit (pkgs.lib) fold composeExtensions concatMap attrValues;
+
+  hls = pkgs.haskell-language-server.override {
+    supportedGhcVersions = [ "96" ];
+  };
+
+  combineOverrides = old:
+    fold composeExtensions (old.overrides or (_: _: { }));
+
+  testConfigurations =
+    let
+      makeTestConfiguration = { ghcVersion, overrides ? new: old: { } }:
+        let
+          inherit (pkgs.haskell.lib) dontCheck packageSourceOverrides;
+        in
+        (pkgs.haskell.packages.${ghcVersion}.override (old: {
+          overrides =
+            combineOverrides old [
+              (packageSourceOverrides { ascii-case = ../ascii-case; })
+              overrides
+            ];
+        })).ascii-case;
+    in
+    rec {
+      ghc-9-0 = makeTestConfiguration {
+        ghcVersion = "ghc90";
+      };
+      ghc-9-2 = makeTestConfiguration {
+        ghcVersion = "ghc92";
+      };
+      ghc-9-4 = makeTestConfiguration {
+        ghcVersion = "ghc94";
+      };
+      ghc-9-6 = makeTestConfiguration {
+        ghcVersion = "ghc96";
+        overrides = new: old: {
+          ascii-char = new.callPackage ./haskell/ascii-char.nix { };
+        };
+      };
+      all = pkgs.symlinkJoin {
+        name = "ascii-case-tests";
+        paths = [ ghc-9-0 ghc-9-2 ghc-9-4 ghc-9-6 ];
+      };
+    };
+
+in
+{
+
+  packages = { inherit testConfigurations; };
+
+  devShells.default = pkgs.mkShell {
+    inputsFrom = [ testConfigurations.ghc-9-6.env ];
+    buildInputs = [ hls pkgs.cabal-install ];
+  };
+
+}
